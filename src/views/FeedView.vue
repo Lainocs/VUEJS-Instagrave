2c170f948058ec7a7e8cd29da78452ca955112d0
Nicolas de Garrigues <ndegarrigues@gmail.com>
2022-03-22 18:22:13 +0100

    :construction: FEAT display posts

--- src/views/FeedView.vue
+++ src/views/FeedView.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="home">
-    feed
+    <div class=" shadow w-3/4 h-screen mx-auto p-1 text-center">
+      <div v-for="post in posts" :key="post.id" class="w-2/4 my-12 mx-auto h-32 bg-gray-200 flex items-center justify-center">
+        <h3>{{post.user_id}}</h3>
+        <p>{{post.text}}</p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -8,5 +13,27 @@
 
 export default {
   name: 'FeedView',
+  data() {
+    return {
+      posts: [],
+      users: []
+    }
+  },
+  methods: {
+      async getAllArticles() {
+        const { data, error } = await this.$supabase
+          .from("posts")
+          .select("*")
+        if(data) {
+          this.posts = data
+        } else {
+          console.log(error)
+        }
+
+      },
+  },
+  created() {
+    this.getAllArticles()
+  },
 }
 </script>
